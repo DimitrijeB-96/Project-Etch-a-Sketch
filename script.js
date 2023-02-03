c02e6fa064d20612b8a98ed98e655bfa0cad3d05
DimitrijeB-96 <mitabackalic96@gmail.com>
2023-02-03 15:45:24 +0100

    color adding option

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
 const gridSize = 256;
-chooseGridSize();
+
+const buttonsDiv = document.createElement('div');
+const gridSizeBtn = document.createElement('button');
+const resetColorBtn = document.createElement('button');
+const redColorBtn = document.createElement('button');
+const blueColorBtn = document.createElement('button');
+const yellowColorBtn = document.createElement('button');
+const greenColorBtn = document.createElement('button');
+
+buttons();
+
+//function that make div with buttons
+function buttons() {
+    
+    //create and append div
+    document.body.appendChild(buttonsDiv);
+
+    //create and append gridSize button
+    gridSizeBtn.setAttribute('class', 'grid-size-button');
+    gridSizeBtn.textContent = 'Set Size';
+    buttonsDiv.appendChild(gridSizeBtn);
+
+    //create and append resetColor button
+    resetColorBtn.setAttribute('class', 'reset-color-button');
+    resetColorBtn.textContent = 'Reset';
+    buttonsDiv.appendChild(resetColorBtn);
+
+    //creating div for buttons that sets the color
+    const colorDiv = document.createElement('div');
+    buttonsDiv.appendChild(colorDiv);
+
+    //red
+    redColorBtn.setAttribute('class', 'red');
+    redColorBtn.textContent = 'Red';
+    colorDiv.appendChild(redColorBtn);
+
+    //blue
+    blueColorBtn.setAttribute('class', 'blue');
+    blueColorBtn.textContent = 'Blue';
+    colorDiv.appendChild(blueColorBtn);
+
+    //green
+    greenColorBtn.setAttribute('class', 'green');
+    greenColorBtn.textContent = 'Green';
+    colorDiv.appendChild(greenColorBtn);
+
+    //yellow
+    yellowColorBtn.setAttribute('class', 'yellow');
+    yellowColorBtn.textContent = 'Yellow';
+    colorDiv.appendChild(yellowColorBtn);
+
+}
 
 //Main div container for grid
 const mainDiv = document.createElement('div');
@@ -28,24 +79,49 @@ function createGrid(gridSize) {
 
 function selectSquare() {
     let eachDiv = document.querySelectorAll('.box');
+    let whichColor = 'white-color';
+
+    if (document.getElementsByName(redColorBtn).clicked == true) {
+        redColorBtn.addEventListener('click', () => {
+            whichColor = 'red-color';
+        });
+    }
+    
+
 
     for(let i = 0; i < eachDiv.length; i++) {
         eachDiv[i].addEventListener('mouseover', () => {
-            eachDiv[i].classList.add('yellow');
+            eachDiv[i].classList.add(whichColor);
+        });
+
+        //Reset button click removes yellow class from "squares"
+        resetColorBtn.addEventListener('click', () => {
+            eachDiv[i].classList.remove(whichColor);
         });
     }
 }
 
-function chooseGridSize()  {
-    const gridSizeBtn = document.createElement('button');
-    gridSizeBtn.setAttribute('class', 'grid-size-button');
-    gridSizeBtn.textContent = 'set size';
-    document.body.appendChild(gridSizeBtn);
+// EVENTS //
 
-    addEventListener('click', () => {
-        //alert();
-    });
-}
+gridSizeBtn.addEventListener('click', () => {
+    alert();
+});
+
+redColorBtn.addEventListener('click', () => {
+
+});
+
+blueColorBtn.addEventListener('click', () => {
+
+});
+
+yellowColorBtn.addEventListener('click', () => {
+
+});
+
+greenColorBtn.addEventListener('click', () => {
+
+});
 
 
 
